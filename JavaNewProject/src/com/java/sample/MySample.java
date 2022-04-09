88ec7007d1def9d1556e651c69d66774dca50345
Sai Vyshnavi Dakarapu <selenium.onlinetutorials@gmail.com>
2022-04-09 12:21:26 +0530

    updated the MySample java file with for loop to print multiples of 20 interating for 100 times

--- JavaNewProject/src/com/java/sample/MySample.java
+++ JavaNewProject/src/com/java/sample/MySample.java
@@ -3,6 +3,9 @@ package com.java.sample;
 public class MySample {
 	public static void main(String[] args) {
 		System.out.println("this is sample java code");
+		for(int i=1;i<100;i++){
+			System.out.println(i*20);
+		}
 	}
 
 }
